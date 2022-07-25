087851cc40ad689808c4febcfbcbb2cfcf62c75e
SokolovMichael <unknownpersons@mail.ru>
2022-07-25 20:21:51 +0300

    Правки после код ревью

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,21 +1,23 @@
-const pageHeader = document.querySelector('.page__header');
-const navMain = document.querySelector('.nav');
-const navToggle = navMain.querySelector('.nav__toggle--menu');
-
-const onNavMenuOpen = () => {
-  if (navMain.classList.contains('nav--closed')) {
-    navMain.classList.remove('nav--closed');
-    navMain.classList.add('nav--opened');
-  } else {
-    navMain.classList.add('nav--closed');
-    navMain.classList.remove('nav--opened');
-  }
-};
-
 const openMenu = () => {
-  pageHeader.classList.remove('page__header--nojs');
-  navMain.classList.remove('nav--nojs');
-  navToggle.addEventListener('click', onNavMenuOpen);
+  if (document.querySelector('.page__header')) {
+    const pageHeader = document.querySelector('.page__header');
+    const navMain = document.querySelector('.nav');
+    const navToggle = navMain.querySelector('.nav__toggle--menu');
+
+    const onNavMenuOpen = () => {
+      if (navMain.classList.contains('nav--closed')) {
+        navMain.classList.remove('nav--closed');
+        navMain.classList.add('nav--opened');
+      } else {
+        navMain.classList.add('nav--closed');
+        navMain.classList.remove('nav--opened');
+      }
+    };
+
+    pageHeader.classList.remove('page__header--nojs');
+    navMain.classList.remove('nav--nojs');
+    navToggle.addEventListener('click', onNavMenuOpen);
+  }
 };
 
 export {openMenu};
